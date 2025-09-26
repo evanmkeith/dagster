9a9a12610ec23a7e1328578d0cf17ca93dabb4ef
Evan Keith <evan.keith@dagsterlabs.com>
2025-09-26 09:44:35 -0700

    add asstes to definitions.py

--- src/dagster_quickstart/definitions.py
+++ src/dagster_quickstart/definitions.py
@@ -1,8 +1,30 @@
-from pathlib import Path
+# from pathlib import Path
 
-from dagster import definitions, load_from_defs_folder
+# from dagster import definitions, load_from_defs_folder
 
 
-@definitions
-def defs():
-    return load_from_defs_folder(path_within_project=Path(__file__))
+# @definitions
+# def defs():
+#     return load_from_defs_folder(path_within_project=Path(__file__))
+
+import pandas as pd
+
+import dagster as dg
+
+sample_data_file = "src/dagster_quickstart/defs/data/sample_data.csv"
+processed_data_file = "src/dagster_quickstart/defs/data/processed_data.csv"
+
+
+@dg.asset
+def processed_data():
+    ## Read data from the CSV
+    df = pd.read_csv(sample_data_file)
+
+    ## Add an age_group column based on the value of age
+    df["age_group"] = pd.cut(
+        df["age"], bins=[0, 30, 40, 100], labels=["Young", "Middle", "Senior"]
+    )
+
+    ## Save processed data
+    df.to_csv(processed_data_file, index=False)
+    return "Data loaded successfully"
